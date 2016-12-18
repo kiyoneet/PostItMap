51bc797d704296554ed5a7d1f31a73c1a9d27001
takai yasumasa <m.e.t.e.o.r.07@gmail.com>
2016-12-18 20:26:06 +0900

    login

--- html/login/login.php
+++ html/login/login.php
@@ -7,27 +7,52 @@ $error = new Error();
 
 var_dump($_POST);
 
+if (isset($_POST)){
+    if (empty($_POST['emailaddress'])){
+        $error->set('メールアドレスが未入力です。');
+    }
+    if (empty($_POST['password'])){
+        $error->set('パスワードが未入力です。');
+    }
 
+    if (count($error->get()) > 0){
+        header('Location : ../login', true);   
+        exit(); 
+    }else{
+        //ログインオブジェクト発行
+        if(!$login = Login_Base::login($_POST["email_address"],$_POST["password"])){
+            $error -> set('メールアドレス、もしくはパスワードが違います。');
+            exit();
+        } else{
+            header('Location : ../menu', true);   
+            exit();
+        }
+    }
 
-if (!empty($_POST["email_address"]) && !empty($_POST["password"])){
-//var_dump($_POST["pass"],$_POST["username"]);
-    $login = Login_Base::login($_POST["email_address"],$_POST["password"]);
-} else{
-    $error -> set('メールアドレス、もしくはパスワードが違います。');
-	header('Location : ../login', true);   
-    exit();   
+    
 }
+
+
+
+// if (!empty($_POST["email_address"]) && !empty($_POST["password"])){
+// //var_dump($_POST["pass"],$_POST["username"]);
+//     $login = Login_Base::login($_POST["email_address"],$_POST["password"]);
+// } else{
+//     $error -> set('メールアドレス、もしくはパスワードが違います。');
+// 	header('Location : ../login', true);   
+//     exit();   
+// }
 	
 
-if ($login = Login_Base::getLoginStatus()){
+// if ($login = Login_Base::getLoginStatus()){
 
-header('Location : ../menu', true);   
-    exit();   
+// header('Location : ../menu', true);   
+//     exit();   
 
 
-}else{
-	$error -> set('メールアドレス、もしくはパスワードが違います。');
-	header('Location : ../login', true);   
-    exit();   
+// }else{
+// 	$error -> set('メールアドレス、もしくはパスワードが違います。');
+// 	header('Location : ../login', true);   
+//     exit();   
 
-}
+// }
