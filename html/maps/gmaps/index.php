cdeaf1c8af3ec91816bee807dd6245917ed48d28
takai yasumasa <y.takai@nac-care.co.jp>
2016-12-20 10:07:08 +0900

    aaa

--- html/maps/gmaps/index.php
+++ html/maps/gmaps/index.php
@@ -7,8 +7,7 @@
   <!-- jQuery -->
   <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
   <!-- GoogleMapAPIKey -->
-  <!--<script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?key=AIzaSyDVbti9y3mOBfGjeOwSRA52zx4I7vnLgD4&sensor=FALSE"></script>-->
-  <script type="text/javascript" src="https://maps.google.com/maps/api/js?sensor=true"></script>
+  <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDVbti9y3mOBfGjeOwSRA52zx4I7vnLgD4&sensor=FALSE"></script>
   <!--gMapsLib -->
   <script type="text/javascript" src="../../../lib/js/gmaps/gmaps.js"></script>
   <!-- gMapsCode -->
