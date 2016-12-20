43d7a129bb7a2a3ccc66be694b8c391860d684ea
takai yasumasa <y.takai@nac-care.co.jp>
2016-12-20 10:02:56 +0900

    test

--- html/maps/gmaps/index.php
+++ html/maps/gmaps/index.php
@@ -5,9 +5,10 @@
   <meta charset="UTF-8">
   <title>GmapsLibExample</title>
   <!-- jQuery -->
-  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
+  <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
   <!-- GoogleMapAPIKey -->
-  <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDVbti9y3mOBfGjeOwSRA52zx4I7vnLgD4&sensor=FALSE"></script>
+  <!--<script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?key=AIzaSyDVbti9y3mOBfGjeOwSRA52zx4I7vnLgD4&sensor=FALSE"></script>-->
+  <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=true"></script>
   <!--gMapsLib -->
   <script type="text/javascript" src="../../../lib/js/gmaps/gmaps.js"></script>
   <!-- gMapsCode -->
